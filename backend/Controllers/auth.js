338755ed50fd5e56172de4e33ce6cc678d6d01eb
vijaykrishna483 <vijaykrshns@gmail.com>
2025-03-10 02:47:43 +0530

    FDFD

--- backend/Controllers/auth.js
+++ backend/Controllers/auth.js
@@ -32,7 +32,7 @@ const register = async (req, res) => {
             secure:process.env.NODE_ENV==='production',
             sameSite:process.env.NODE_ENV==='production'? 'none':'strict',
             maxAge:7 * 24 * 60 * 60* 1000
-        });
+        })
        
        if (user.isAccountVerified) {
         return res.json({success:"false", message: 'already verified' });
--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -6,14 +6,14 @@ const userAuth =async (req,res,next)=>{
     const {token}=req.cookies;
 
     if(!token){
-        return res.json({success:false,message:'Not uathorsed login again'})
+        return res.json({success:false,message:'Not authorised login again'})
     }
     try{
      const tokenDecode=jwt.verify(token,process.env.JWT_SECRET)
      if(tokenDecode.id){
         req.body.userID=tokenDecode.id
      }else{
-        return res.json({success:false,message:'Not uathorsed login again'})
+        return res.json({success:false,message:'Not authorised login id'})
 
      }
      next()
